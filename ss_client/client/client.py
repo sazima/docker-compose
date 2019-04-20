061db8fcccf405a5c43076b6ba7acec20745459e
wukt <w@wktadmin.com>
2019-04-20 22:41:47 +0800

    update

--- ss_client/client/client.py
+++ ss_client/client/client.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python
-# import shadowsocks
-
-import os
+from gevent import monkey
+monkey.patch_all()
+import gevent
 import json
-import time
-import threading
-import traceback
+import sys
+
+from shadowsocks.local import main as local_main
 
 
-def remove_file(*files):
-    time.sleep(5)
-    for file in files:
-        os.remove(file)
+def start_local(config):
+    sys.argv = ('sslocal -c {} start'.format(config)).split()
+    print(sys.argv)
+    sys.exit(local_main())
 
 
 def run():
     with open('./config.json', 'r') as rf:
         client_datas = json.load(rf)
-    command = ''  # run sslocal
-    file_names = []  # temp json_file
+
     for index, client_data in enumerate(client_datas):
         file_name = '/tmp/config{}.json'.format(index)
+        print(file_name)
         with open(file_name, 'w') as wf:
             json.dump(client_data, wf)
-        command += "sslocal -c {} restart & ".format(file_name)
-        file_names.append(file_name)
-    remove = threading.Thread(target=remove_file, args=file_names)
-    remove.start()
-    os.popen(command.strip('&'))
-    return command.strip('&')
+        yield file_name
 
 
 if __name__ == '__main__':
-    while True:
-        command = run()
-        time.sleep(3600 * 24)  # restart one day
+    for file_name in run():
+        task = gevent.spawn(start_local, file_name)
+    task.join()
+
 
